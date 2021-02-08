b2b3f24e93f796ec65e3e725acf238c4154a477d
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-08 13:47:17 +0100

    Update init-kubernetes-leader.sh

--- bin/init-kubernetes-leader.sh
+++ bin/init-kubernetes-leader.sh
@@ -5,14 +5,15 @@
 #########################################################################
 set -x                                                                  ;
 #########################################################################
-ip_leader=$( ip r | grep -v docker | awk '/kernel/{ print $9 }' )       ;
-kube=kube-apiserver                                                     ;
-log=/tmp/install-leader.log                                             ;
-sleep=10                                                                ;
+test -n "${calico}"             || exit 100                             ;
+test -n "${file}"               || exit 100                             ;
+test -n "${ip_leader}"          || exit 101                             ;
+test -n "${kube}"               || exit 102                             ;
+test -n "${pod_network_cidr}"   || exit 103                             ;
 #########################################################################
-calico=https://docs.projectcalico.org/v3.17/manifests/calico.yaml       ;
-pod_network_cidr=192.168.0.0/16                                         ;
 kubeconfig=/etc/kubernetes/admin.conf                                   ;
+log=/tmp/${file}.log                                                    ;
+sleep=10                                                                ;
 #########################################################################
 while true                                                              ;
 do                                                                      \
@@ -22,40 +23,40 @@ do                                                                      \
         &&                                                              \
         break                                                           \
                                                                         ;
-        sleep $sleep                                                    ;
+        sleep ${sleep}                                                  ;
 done                                                                    ;
 #########################################################################
-echo $ip_leader $kube                                                   \
+echo ${ip_leader} ${kube}                                               \
 |                                                                       \
 sudo tee --append /etc/hosts                                            ;
 sudo swapoff --all                                                      ;
 sudo kubeadm init                                                       \
         --upload-certs                                                  \
         --control-plane-endpoint                                        \
-                "$kube"                                                 \
+                "${kube}"                                               \
         --pod-network-cidr                                              \
-                $pod_network_cidr                                       \
+                ${pod_network_cidr}                                     \
         --ignore-preflight-errors                                       \
                 all                                                     \
-        2>&1                                                            \
+        2>& 1                                                           \
 |                                                                       \
-tee --append $log                                                       ;
+tee --append ${log}                                                     ;
 #########################################################################
 sudo kubectl apply                                                      \
         --filename                                                      \
-                $calico                                                 \
+                ${calico}                                               \
         --kubeconfig                                                    \
-                $kubeconfig                                             \
-        2>&1                                                            \
+                ${kubeconfig}                                           \
+        2>& 1                                                           \
 |                                                                       \
-tee --append $log                                                       ;
+tee --append ${log}                                                     ;
 #########################################################################
-mkdir -p $HOME/.kube                                                    ;
-sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config                   ;
-sudo chown -R $(id -u):$(id -g) $HOME/.kube/                            ;
-echo 'source <(kubectl completion bash)'                                \
+mkdir -p ${HOME}/.kube                                                  ;
+sudo cp /etc/kubernetes/admin.conf ${HOME}/.kube/config                 ;
+sudo chown -R $( id -u ):$( id -g ) ${HOME}/.kube/                      ;
+echo 'source 0<( kubectl completion bash )'                             \
 |                                                                       \
-tee --append $HOME/.bashrc                                              ;
+tee --append ${HOME}/.bashrc                                            ;
 #########################################################################
 while true                                                              ;
 do                                                                      \
@@ -67,13 +68,13 @@ do                                                                      \
         &&                                                              \
         break                                                           \
                                                                         ;
-        sleep $sleep                                                    ;
+        sleep ${sleep}                                                  ;
 done                                                                    ;
 #########################################################################
 sudo sed --in-place                                                     \
-        /$kube/d                                                        \
+        /${kube}/d                                                      \
         /etc/hosts                                                      ;
 sudo sed --in-place                                                     \
-        /127.0.0.1.*localhost/s/$/' '$kube/                             \
+        /127.0.0.1.*localhost/s/$/' '${kube}/                           \
         /etc/hosts                                                      ;
 #########################################################################
