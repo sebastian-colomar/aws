d030fccecc131d7dd9fdc4192329d0fdeabf74c8
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-08 12:37:00 +0100

    Update init-orchestrator.sh

--- bin/init-orchestrator.sh
+++ bin/init-orchestrator.sh
@@ -3,32 +3,15 @@
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
-set +x && test "$debug" = true && set -x				;
+set -x
 #########################################################################
-test -n "$A"	                && export A                 || exit 301 ;
-test -n "$branch_docker_aws"	&& export branch_docker_aws || exit 302 ;
-test -n "$debug" 		&& export debug	            || exit 303	;
-test -n "$domain" 		&& export domain	    || exit 304	;
-test -n "$HostedZoneName"	&& export HostedZoneName    || exit 305 ;
-test -n "$RecordSetNameKube"	&& export RecordSetNameKube || exit 306 ;
-test -n "$mode"                 && export mode	            || exit 307	;
-test -n "$stack"                && export stack	            || exit 308	;
+file=common-functions.sh
+path=lib
 #########################################################################
-file=common-functions.sh						;
-path=lib                                 				;
+source ./${path}/${file}
 #########################################################################
-source ./$path/$file                                                    ;
+file=init-${mode}.sh
+path=bin
 #########################################################################
-export -f encode_string							;
-export -f exec_remote_file						;
-export -f send_command							;
-export -f send_list_command						;
-export -f send_remote_file						;
-export -f send_wait_targets						;
-export -f service_wait_targets						;
-#########################################################################
-file=init-$mode.sh                                                      ;
-path=bin								;
-#########################################################################
-./$path/$file                                                           ;
+source ./${path}/${file}
 #########################################################################
