0109933c1938be6abf766203b40a6336f391c4ae
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-08 11:32:50 +0100

    Update common-functions.sh

--- lib/common-functions.sh
+++ lib/common-functions.sh
@@ -1,10 +1,9 @@
 #########################################################################
-#	./lib/common-functions.sh                                       #
-#########################################################################
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
-function encode_string {						\
+function _encode_string {						\
+                                                                        #
   local string="$1"							;
   echo -n $string                                                 	\
   |     	                                                        \
@@ -15,81 +14,90 @@ function encode_string {						\
 									;
 }									;
 #########################################################################
-function exec_remote_file {						\
+function _exec_remote_file {						\
                                                                         #
-  local domain=$1							;
+  local branch=$1							;
   local file=$2								;
   local path=$3								;
+  local url=$4								;
                                                                         #
   local uuid=$( uuidgen )						;
   path=$uuid/$path                                                      ;
                                                                         #
   git clone                                                             \
-        --single-branch --branch $branch_docker_aws                     \
-        https://$domain/$A                                              \
+        --single-branch --branch $branch				\
+	https://$url		                                        \
         $uuid                                                           \
-                                                                        ;
+									;
+  chmod +x ./$path/$file						;
   ./$path/$file								;
   rm --force --recursive $uuid                                          ;
                                                                         #
 }									;
 #########################################################################
-function send_command {							\
+function _send_command {						\
+                                                                        #
   local command="$1" 							;
   local stack="$2" 							;
   local target="$3" 							;
+                                                                        #
   aws ssm send-command 							\
     --document-name "AWS-RunShellScript" 				\
+    --output text 							\
     --parameters commands="$command" 					\
+    --query "Command.CommandId" 					\
     --targets 								\
       Key=tag:"aws:cloudformation:stack-name",Values="$stack" 		\
       Key=tag:"aws:cloudformation:logical-id",Values="$target" 		\
-    --query "Command.CommandId" 					\
-    --output text 							\
    									;
 }									;
 #########################################################################
-function send_list_command {						\
+function _send_list_command {						\
+                                                                        #
   local command="$1" 							;
   local sleep=$2							;
   local stack=$3 							;
   local target=$4 							;
+                                                                        #
   local CommandId=$( 							\
-    send_command "$command" $stack $target				\
+    _send_command "$command" $stack $target				\
   ) 									;
+                                                                        #
   while true 								;
   do									\
     sleep $sleep							;
     aws ssm list-command-invocations 					\
       --command-id $CommandId 						\
-      --query "CommandInvocations[].CommandPlugins[].Output" 		\
       --details 							\
       --output text 							\
+      --query "CommandInvocations[].CommandPlugins[].Output" 		\
     | 									\
     grep [a-zA-Z0-9] && break						;
   done 									;
+                                                                        #
 }									;
 #########################################################################
-function send_list_targets {						\
+function _send_list_command_targets {					\
   local command="$1"							;
   local sleep=$2							;
   local stack=$3							;
   local targets="$4"							;
   for target in $targets                                                ;
   do                                                                    \
-    send_list_command "$command" $sleep $stack $target			;
+    _send_list_command "$command" $sleep $stack $target			;
   done                                                                  ;
 }									;
 #########################################################################
-function send_remote_file {						\
+function _send_list_command_remote {					\
                                                                         #
-  local domain=$1							;
+  local branch=$1
   local export="$2"							;
   local file=$3								;
   local path=$4								;
   local sleep=$5							;
   local stack=$6							;
   local targets="$7"							;
+  local url=$8								;
                                                                         #
   local uuid=$( uuidgen )						;
   path=$uuid/$path                                                      ;
@@ -100,10 +108,11 @@ function send_remote_file {						\
     path=$path                                                          \
     &&									\
     git clone                                                           \
-      --single-branch --branch $branch_docker_aws                     	\
-      https://$domain/$A                                              	\
+      --single-branch --branch $branch		                     	\
+      https://$url                                              	\
       $uuid                                                           	\
     &&									\
+    chmod +x ./$path/$file						\
     ./$path/$file                                                       \
       2>&1                                                    		\
     |                                                               	\
@@ -114,35 +123,42 @@ function send_remote_file {						\
                                                                         #
   for target in $targets                                                ;
   do                                                                    \
-    send_list_command "$command" $sleep $stack $target			;
+    _send_list_command "$command" $sleep $stack $target			;
   done                                                                  ;
                                                                         #
 }									;
 #########################################################################
-function send_wait_targets {						\
+function _send_list_command_targets_wait {				\
+                                                                        #
   local command="$1"							;
   local sleep=$2							;
   local stack=$3							;
   local targets="$4"							;
+                                                                        #
   for target in $targets                                                ;
   do                                                                    \
     while true								;
     do									\
-      output="$( send_list_command "$command" $sleep $stack $target )"	;
+      output="$( _send_list_command "$command" $sleep $stack $target )"	;
       echo "$output" | grep ERROR && continue				;
       echo "$output" | grep [a-zA-Z0-9] && break			;
     done								;
   done                                                                  ;
+                                                                        #
 }									;
 #########################################################################
-function service_wait_targets {						\
+function _wait_service_targets {					\
+                                                                        #
   local service=$1							;
   local sleep=$2							;
   local stack=$3							;
   local targets="$4"							;
-  command="                                                             \
+                                                                        #
+  local command="                                                       \
     service $service status 2> /dev/null | grep running			\
   "                                                                     ;
-  send_wait_targets "$command" $sleep $stack "$targets"                 ; 
+                                                                        #
+  _send_list_command_targets_wait "$command" $sleep $stack "$targets"   ;
+                                                                        #
 }									;
 #########################################################################
