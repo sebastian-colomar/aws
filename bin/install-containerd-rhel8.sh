5ebe50eab189383e8724221a66f5943374b107fd
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-10 17:55:20 +0100

    Create install-containerd-rhel8.sh

---
+++ bin/install-containerd-rhel8.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#########################################################################
+#      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
+#      SPDX-License-Identifier:  GPL-2.0-only                           #
+#########################################################################
+set -x                                                                  ;
+#########################################################################
+test -n "${engine}"		|| exit 100                             ;
+#########################################################################
+baseurl=https://packages.cloud.google.com				;
+engine=containerd							;
+repo=https://download.docker.com/linux/centos/docker-ce.repo		;
+rpm_key=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg	;
+sleep=10                                                                ;
+version="1.18.14-00"                                                    ;
+yum_key=https://packages.cloud.google.com/yum/doc/yum-key.gpg		;
+#########################################################################
+sudo tee /etc/modules-load.d/${engine}.conf <<EOF
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay							;
+sudo modprobe br_netfilter						;
+#########################################################################
+sudo tee /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system							;
+#########################################################################
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2	;
+sudo yum-config-manager --add-repo ${repo}				;
+sudo yum install -y ${engine}.io					;
+sudo mkdir -p /etc/containerd						;
+containerd config default | sudo tee /etc/containerd/config.toml	;
+sudo systemctl restart ${engine}					;
+sudo systemctl enable --now ${engine}					;
+#########################################################################
+while true                                                              ;
+do                                                                      \
+        systemctl status ${engine}                                      \
+        |                                                               \
+        grep running                                                    \
+        &&                                                              \
+        break                                                           \
+                                                                        ;
+        sleep $sleep                                                    ;
+done                                                                    ;
+#########################################################################
