12a02b020708a341cef5d1fd222a972bdd8bc281
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-10 04:19:49 +0100

    Update install-kubelet-ubuntu18.sh

--- bin/install-kubelet-ubuntu18.sh
+++ bin/install-kubelet-ubuntu18.sh
@@ -5,8 +5,7 @@
 #########################################################################
 set -x                                                                  ;
 #########################################################################
-engine=docker                                                           ;
-log=/tmp/install-kubelet-ubuntu18.sh.log                                ;
+test -n "${engine}"		|| exit 100                             ;
 #########################################################################
 sleep=10                                                                ;
 version="1.18.14-00"                                                    ;
@@ -23,24 +22,15 @@ done                                                                    ;
 #########################################################################
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg           \
 |                                                                       \
-sudo apt-key add -                                                      \
-        2>& 1                                                           \
-|                                                                       \
-tee --append ${log}                                                     ;
+sudo apt-key add -                                                      ;
 echo deb http://apt.kubernetes.io/ kubernetes-xenial main               \
 |                                                                       \
 sudo tee -a /etc/apt/sources.list.d/kubernetes.list                     ;
-sudo apt-get update                                                     \
-        2>& 1                                                           \
-|                                                                       \
-tee --append ${log}                                                     ;
+sudo apt-get update                                                     ;
 sudo apt-get install -y --allow-downgrades                              \
-        kubelet=${version} kubeadm=${version} kubectl=${version}        \
-        2>& 1                                                           \
-|                                                                       \
-tee --append ${log}                                                     ;
-sudo systemctl enable --now kubelet                                     \
-        2>& 1                                                           \
-|                                                                       \
-tee --append ${log}                                                     ;
+        kubeadm=${version}                                              \
+        kubectl=${version}                                              \
+        kubelet=${version}                                              \
+                                                                        ;
+sudo systemctl enable --now kubelet                                     ;
 #########################################################################
