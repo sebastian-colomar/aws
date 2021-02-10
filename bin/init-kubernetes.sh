e803ac70966257c797701d3f92f0d492e1c82ec1
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-10 05:55:27 +0100

    Update init-kubernetes.sh

--- bin/init-kubernetes.sh
+++ bin/init-kubernetes.sh
@@ -5,34 +5,30 @@
 #########################################################################
 set -x;
 #########################################################################
-test -n "${engine}"		|| exit 100                             ;
-test -n "${file}"               || exit 101                             ;
+test -n "${engine}"		|| exit 101                             ;
 test -n "${HostedZoneName}"	|| exit 102                             ;
 test -n "${ip_leader}"          || exit 103                             ;
-test -n "${stack}"		|| exit 104                             ;
+test -n "${ip_master1}"		|| exit 104                             ;
+test -n "${ip_master2}"		|| exit 105                             ;
+test -n "${ip_master3}"		|| exit 106                             ;
+test -n "${mode}"		|| exit 107                             ;
+test -n "${os}"			|| exit 108                             ;
+test -n "${stack}"		|| exit 109                             ;
 #########################################################################
-domain=github.com
-username=academiaonline
-repository=aws
-branch=main
-
+branch=main								;
 calico=https://docs.projectcalico.org/v3.17/manifests/calico.yaml       ;
+domain=github.com							;
+path=bin								;
 pod_network_cidr=192.168.0.0/16                                         ;
+RecordSetNameKube=kube-apiserver					;
+repository=aws								;
+sleep=10								;
+username=academiaonline							;
+#########################################################################
+kube=${RecordSetNameKube}.${HostedZoneName}				;
+url=${domain}/${username}/${repository}					;
 #########################################################################
 export=" 								\
-"									;
-RecordSetNameKube=kube-apiserver
-kube=${RecordSetNameKube}.${HostedZoneName};
-log=/tmp/${file}.log;
-path=bin;
-sleep=10;
-
-#########################################################################
-url=${domain}/${username}/${repository};
-#########################################################################
-export=" 								\
-  ${export}								\
-  && 									\
   export engine=${engine} 						\
 "									;
 targets="								\
@@ -44,17 +40,22 @@ targets="								\
 	InstanceWorker3							\
 "									;
 #########################################################################
-for service in ${engine} kubelet	;
-	do \
-		file=install-${service}-{os}.sh	;
-		_send_list_command_remote ${branch} "" ${file} ${path} ${sleep} ${stack} "${targets}" ${url};
-	done	;
+for service in ${engine} kubelet					;
+	do 								\
+		file=install-${service}-{os}.sh				;
+		_send_list_command_remote 				\
+			${branch} 					\
+			"" 						\
+			${file} 					\
+			${path} 					\
+			${sleep} 					\
+			${stack} 					\
+			"${targets}" 					\
+			${url} 						\
+									;
+	done								;
 #########################################################################
 export=" 								\
-"									;
-export=" 								\
-  $export								\
-  && 									\
   export calico=${calico} 						\
   && 									\
   export ip_leader=${ip_leader}						\
@@ -63,99 +64,140 @@ export=" 								\
   && 									\
   export pod_network_cidr=${pod_network_cidr} 				\
 "									;
+role=leader								;
 targets="								\
 	InstanceMaster1							\
 "									;
 #########################################################################
-role=leader
-file=init-${mode}-${role}.sh;
-_send_list_command_remote ${branch} "${export}" ${file} ${path} ${sleep} ${stack} "${targets}" ${url};
+file=init-${mode}-${role}.sh						;
+log=/tmp/${file}.log							;
+#########################################################################
+_send_list_command_remote 						\
+	${branch} 							\
+	"${export}" 							\
+	${file} 							\
+	${path} 							\
+	${sleep} 							\
+	${stack} 							\
+	"${targets}" 							\
+	${url} 								\
+									;
 #########################################################################
 command="								\
 	grep --max-count 1						\
 		certificate-key						\
-		$log							\
+		${log}							\
 "									;
 token_certificate=$(							\
-  encode_string "							\
-    $(									\
-      _send_list_command_targets_wait "$command" $sleep $stack "$targets" \
-    )									\
-  "									;	
+	encode_string "							\
+		$(							\
+			_send_list_command_targets_wait 		\
+				"${command}" 				\
+				${sleep} 				\
+				${stack} 				\
+				"${targets}" 				\
+		)							\
+	"								;
 )									;
 #########################################################################
 command="								\
 	grep --max-count 1						\
 		discovery-token-ca-cert-hash				\
-		$log							\
+		${log}							\
 "									;
 token_discovery=$(							\
-  encode_string "							\
-    $(									\
-      _send_list_command_targets_wait "$command" $sleep $stack "$targets" \
-    )									\
-  "									;	
+	encode_string "							\
+		$(							\
+			_send_list_command_targets_wait 		\
+				"${command}" 				\
+				${sleep} 				\
+				${stack} 				\
+				"${targets}" 				\
+		)							\
+	"								;
 )									;
 #########################################################################
 command="								\
 	grep --max-count 1						\
 		kubeadm.*join						\
-		$log							\
+		${log}							\
 "									;
 token_token=$(								\
-  encode_string "							\
-    $(									\
-      _send_list_command_targets_wait "$command" $sleep $stack "$targets" \
-    )									\
-  "									;	
+	encode_string "							\
+		$(							\
+			_send_list_command_targets_wait 		\
+				"${command}" 				\
+				${sleep} 				\
+				${stack} 				\
+				"${targets}" 				\
+		)							\
+	"								;
 )									;
 #########################################################################
 export=" 								\
-"									;
-export=" 								\
-  $export								\
-  &&									\
   export ip_leader=${ip_leader}						\
   && 									\
   export kube=${kube}							\
   && 									\
-  export token_certificate=$token_certificate				\
+  export token_certificate=${token_certificate}				\
   &&									\
-  export token_discovery=$token_discovery				\
+  export token_discovery=${token_discovery}				\
   &&									\
-  export token_token=$token_token					\
+  export token_token=${token_token}					\
 "									;
-role=master
-file=init-${mode}-${role}.sh;
+role=master								;
 targets="								\
 	InstanceMaster2							\
 	InstanceMaster3							\
 "									;
-_send_list_command_remote ${branch} "${export}" ${file} ${path} ${sleep} ${stack} "${targets}" ${url};
 #########################################################################
-unset token_certificate							;
+file=init-${mode}-${role}.sh						;
+#########################################################################
+_send_list_command_remote 						\
+	${branch} 							\
+	"${export}" 							\
+	${file} 							\
+	${path} 							\
+	${sleep} 							\
+	${stack} 							\
+	"${targets}" 							\
+	${url} 								\
+									;
 #########################################################################
 export=" 								\
-"									;
-export=" 								\
-  $export								\
-  &&									\
-  export ip_leader=${ip_leader}						\
+  export ip_master1=${ip_master1}					\
+  && 									\
+  export ip_master2=${ip_master2}					\
+  && 									\
+  export ip_master3=${ip_master3}					\
   && 									\
   export kube=${kube}							\
   && 									\
-  export token_certificate=$token_certificate				\
+  export token_discovery=${token_discovery}				\
   &&									\
-  export token_discovery=$token_discovery				\
-  &&									\
-  export token_token=$token_token					\
+  export token_token=${token_token}					\
 "									;
 role=worker;
-test ${engine} == docker && file=init-${mode}-${role}-${engine}.sh || file=init-${mode}-${role}.sh;
 targets="								\
 	InstanceWorker1							\
 	InstanceWorker2							\
 	InstanceWorker3							\
 "									;
-_send_list_command_remote ${branch} "${export}" ${file} ${path} ${sleep} ${stack} "${targets}" ${url};
+#########################################################################
+test ${engine} == docker 						\
+&& 									\
+file=init-${mode}-${role}-${engine}.sh 					\
+|| 									\
+file=init-${mode}-${role}.sh						;
+#########################################################################
+_send_list_command_remote 						\
+	${branch} 							\
+	"${export}" 							\
+	${file} 							\
+	${path} 							\
+	${sleep} 							\
+	${stack} 							\
+	"${targets}" 							\
+	${url} 								\
+									;
 #########################################################################
