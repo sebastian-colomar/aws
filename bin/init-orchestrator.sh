7d68283f0b3c681457a4fd7cddd549229edd7442
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-10 05:59:58 +0100

    Update init-orchestrator.sh

--- bin/init-orchestrator.sh
+++ bin/init-orchestrator.sh
@@ -3,17 +3,17 @@
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
-set -x
+set -x									;
 #########################################################################
-test -n "${engine}" || exit 101;
-test -n "${HostedZoneName}"	|| exit 102;
-test -n "${ip_leader}" || exit 103;
-test -n "${ip_master1}" || exit 104;
-test -n "${ip_master2}" || exit 105;
-test -n "${ip_master3}" || exit 106;
-test -n "${mode}" || exit 107;
-test -n "${os}" || exit 108;
-test -n "${stack}" || exit 109;
+test -n "${engine}"		|| exit 101                             ;
+test -n "${HostedZoneName}"	|| exit 102                             ;
+test -n "${ip_leader}"          || exit 103                             ;
+test -n "${ip_master1}"		|| exit 104                             ;
+test -n "${ip_master2}"		|| exit 105                             ;
+test -n "${ip_master3}"		|| exit 106                             ;
+test -n "${mode}"		|| exit 107                             ;
+test -n "${os}"			|| exit 108                             ;
+test -n "${stack}"		|| exit 109                             ;
 #########################################################################
 file=common-functions.sh
 path=lib
