417cf7d429a258cd592aa38d89c9833d653cd1de
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-14 05:54:04 +0100

    Update install-cri-o-rhel8.sh

--- bin/install-cri-o-rhel8.sh
+++ bin/install-cri-o-rhel8.sh
@@ -28,8 +28,8 @@ sudo sysctl --system							;
 #########################################################################
 sudo curl -L -o ${repo_path}:stable.repo				\
 	${repo_url}:/kubic:/libcontainers:/stable/${OS}/devel:kubic:libcontainers:stable.repo	;
-sudo curl -L -o ${repo_path}:stable:${engine}:${version_major}.repo	\
-	${repo_url}:kubic:libcontainers:stable:${engine}:${version_major}/${OS}/devel:kubic:libcontainers:stable:${engine}:${version_major}.repo	;
+sudo curl -L -o ${repo_path}:stable:${engine}:1.${version_major}.repo	\
+	${repo_url}:kubic:libcontainers:stable:${engine}:1.${version_major}/${OS}/devel:kubic:libcontainers:stable:${engine}:1.${version_major}.repo	;
 sudo yum update -y                                                      ;
 sudo yum install -y ${engine}						;
 sudo systemctl restart ${engine}					;
