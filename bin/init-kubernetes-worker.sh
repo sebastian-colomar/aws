7ad199211fb9aaf484a66aea878c948a33c5e8a3
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-15 22:03:16 +0100

    Update init-kubernetes-worker.sh

--- bin/init-kubernetes-worker.sh
+++ bin/init-kubernetes-worker.sh
@@ -6,19 +6,12 @@
 set -x                                                                  ;
 #########################################################################
 test -n "${InstanceMaster1}"    || exit 301                             ;
-test -n "${InstanceMaster2}"    || exit 302                             ;
-test -n "${InstanceMaster3}"    || exit 303                             ;
 test -n "${kube}"               || exit 304                             ;
 test -n "${token_discovery}"    || exit 305                             ;
 test -n "${token_token}"        || exit 306                             ;
 #########################################################################
-branch=docker                                                           ;
-compose=etc/swarm/manifests/nlb.yaml                                    ;
 log=/tmp/$( uuidgen ).log                                               ;
-port_master=6443                                                        ;
-repository=https://github.com/academiaonline/nlb                        ;
 sleep=10                                                                ;
-uuid=/tmp/$( uuidgen )                                                  ;
 #########################################################################
 echo ${InstanceMaster1} ${kube}                                         \
 |                                                                       \
@@ -62,48 +55,4 @@ do                                                                      \
         break                                                           ;
         sleep ${sleep}                                                  ;
 done                                                                    ;
-#########################################################################
-while true                                                              ;
-do                                                                      \
-        systemctl status docker                                         \
-        |                                                               \
-        grep running                                                    \
-        &&                                                              \
-        break                                                           ;
-        sleep ${sleep}                                                  ;
-done                                                                    ;
-#########################################################################
-git clone                                                               \
-        --single-branch --branch ${branch}                              \
-        ${repository}                                                   \
-        ${uuid}                                                         ;
-sed --in-place s/worker/manager/                                        \
-        ${uuid}/${compose}                                              ;
-sed --in-place s/port_master/${port_master}/                            \
-        ${uuid}/${compose}                                              ;
-sed --in-place s/port_master/${port_master}/                            \
-        ${uuid}/run/secrets/etc/nginx/conf.d/default.conf               ;
-sed --in-place s/ip_master1/${InstanceMaster1}/                         \
-        ${uuid}/run/secrets/etc/nginx/conf.d/default.conf               ;
-sed --in-place s/ip_master2/${InstanceMaster2}/                         \
-        ${uuid}/run/secrets/etc/nginx/conf.d/default.conf               ;
-sed --in-place s/ip_master3/${InstanceMaster3}/                         \
-        ${uuid}/run/secrets/etc/nginx/conf.d/default.conf               ;
-sudo cp --recursive --verbose ${uuid}/run/* /run                        ;
-sudo docker swarm init                                                  ;
-sudo docker stack deploy --compose-file ${uuid}/${compose} nlb          ;
-rm --recursive --force ${uuid}                                          ;
-while true                                                              ;
-do                                                                      \
-        sleep 1                                                         ;
-        sudo docker service ls | grep '\([0-9]\)/\1' && break           ;
-done                                                                    ;
-sudo rm --recursive --force /run/secrets /run/configs                   ;
-#########################################################################
-sudo sed --in-place                                                     \
-        /${kube}/d                                                      \
-        /etc/hosts                                                      ;
-sudo sed --in-place                                                     \
-        /127.0.0.1.*localhost/s/$/' '${kube}/                           \
-        /etc/hosts                                                      ;
 #########################################################################
