941303131185b3dd3afb04dd597c0a73283ffd8e
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-16 10:36:39 +0100

    Update install-containerd-ubuntu18.sh

--- bin/install-containerd-ubuntu18.sh
+++ bin/install-containerd-ubuntu18.sh
@@ -9,22 +9,11 @@ engine=containerd                                                       ;
 #########################################################################
 sudo apt-get update                                                     ;
 sudo apt-get install -y ${engine}                                       ;
+#########################################################################
 sudo mkdir -p /etc/${engine}						;
 sudo mkdir -p /etc/systemd/system/${engine}.service.d                   ;
 containerd config default | sudo tee /etc/containerd/config.toml	;
 #########################################################################
-sudo tee /etc/${engine}/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-#########################################################################
-sudo systemctl daemon-reload                                            ;
 sudo systemctl restart ${engine}					;
 sudo systemctl enable --now ${engine}					;
 #########################################################################
