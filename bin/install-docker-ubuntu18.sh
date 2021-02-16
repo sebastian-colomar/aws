c973155460a8a747125c440464c4d362713880e7
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-16 11:02:40 +0100

    Create install-docker-ubuntu18.sh

---
+++ bin/install-docker-ubuntu18.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#########################################################################
+#      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
+#      SPDX-License-Identifier:  GPL-2.0-only                           #
+#########################################################################
+set -x                                                                  ;
+#########################################################################
+engine=docker                                                           ;
+#########################################################################
+sudo apt-get update                                                     ;
+sudo apt-get install -y ${engine}.io                                    ;
+#########################################################################
+sudo mkdir -p /etc/${engine}						;
+sudo mkdir -p /etc/systemd/system/${engine}.service.d                   ;
+#########################################################################
+sudo tee /etc/${engine}/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+#########################################################################
+sudo systemctl daemon-reload                                            ;
+sudo systemctl restart ${engine}					;
+sudo systemctl enable --now ${engine}					;
+#########################################################################
