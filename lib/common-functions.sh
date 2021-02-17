7b31de93d6882cf3d42ea1cf77f6299677987829
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-17 13:32:10 +0100

    Update common-functions.sh

--- lib/common-functions.sh
+++ lib/common-functions.sh
@@ -4,169 +4,170 @@
 #########################################################################
 function _encode_string {						\
                                                                         #
-  local string="$1"							;
-  echo -n ${string}                                                 	\
-  |     	                                                        \
-  sed 's/\\/ /'                                           		\
-  |                                                       		\
-  base64                                          			\
-    --wrap 0                                				\
+	local string="$1"						;
+	echo -n ${string} 						\
+	|								\
+	sed 's/\\/ /' 							\
+	|								\
+	base64 								\
+		--wrap 0 						\
 									;
 }									;
 #########################################################################
 function _exec_remote_file {						\
                                                                         #
-  local branch=$1							;
-  local file=$2								;
-  local path=$3								;
-  local url=$4								;
+	local branch=$1							;
+	local file=$2							;
+	local path=$3								;
+	local url=$4								;
                                                                         #
-  local uuid=$( uuidgen )						;
+	local uuid=$( uuidgen )						;
                                                                         #
-  path=${uuid}/${path}							;
+	path=${uuid}/${path}							;
                                                                         #
-  git clone                                                             \
-        --single-branch --branch ${branch}				\
-	https://${url}		                                        \
-        ${uuid}                                                         \
+	git clone 							\
+		--single-branch --branch ${branch} 			\
+		https://${url} 						\
+		${uuid} 						\
 									;
-  chmod +x ${path}/${file}						;
-  ${path}/${file}							;
-  rm --force --recursive ${uuid}                                        ;
+	chmod +x ${path}/${file}					;
+	${path}/${file}							;
+	rm --force --recursive ${uuid}					;
                                                                         #
 }									;
 #########################################################################
 function _send_command {						\
                                                                         #
-  local command="$1" 							;
-  local stack="$2" 							;
-  local target="$3" 							;
+	local command="$1"						;
+	local stack="$2"						;
+	local target="$3"						;
                                                                         #
-  aws ssm send-command 							\
-    --document-name "AWS-RunShellScript" 				\
-    --output text 							\
-    --parameters commands="${command}" 					\
-    --query "Command.CommandId" 					\
-    --targets 								\
-      Key=tag:"aws:cloudformation:stack-name",Values="${stack}"		\
-      Key=tag:"aws:cloudformation:logical-id",Values="${target}" 	\
+	aws ssm send-command 						\
+		--document-name "AWS-RunShellScript" 			\
+		--output text 						\
+		--parameters commands="${command}" 			\
+		--query "Command.CommandId" 				\
+		--targets 						\
+	Key=tag:"aws:cloudformation:stack-name",Values="${stack}" 	\
+	Key=tag:"aws:cloudformation:logical-id",Values="${target}" 	\
    									;
 }									;
 #########################################################################
 function _send_list_command {						\
                                                                         #
-  local command="$1" 							;
-  local sleep=$2							;
-  local stack=$3 							;
-  local target=$4 							;
+	local command="$1"						;
+	local sleep=$2							;
+	local stack=$3 							;
+	local target=$4							;
                                                                         #
-  local CommandId=$( 							\
-    _send_command "${command}" ${stack} ${target}			\
-  ) 									;
+	local CommandId=$( 						\
+		_send_command "${command}" ${stack} ${target} 		\
+	)								;
                                                                         #
-  while true 								;
-  do									\
-    sleep ${sleep}							;
-    aws ssm list-command-invocations 					\
-      --command-id ${CommandId} 					\
-      --details 							\
-      --output text 							\
-      --query "CommandInvocations[].CommandPlugins[].Output" 		\
-    | 									\
-    grep [a-zA-Z0-9] && break						;
-  done 									;
+	while true							;
+		do 							\
+			sleep ${sleep}					;
+			aws ssm list-command-invocations 		\
+				--command-id ${CommandId} 		\
+				--details 				\
+				--output text 				\
+				--query 				\
+			"CommandInvocations[].CommandPlugins[].Output" 	\
+			|						\
+			grep [a-zA-Z0-9] && break			;
+		done							;
                                                                         #
 }									;
 #########################################################################
 function _send_list_command_remote {					\
                                                                         #
-  local branch=$1
-  local export="$2"							;
-  local file=$3								;
-  local log=$4								;
-  local path=$5								;
-  local sleep=$6							;
-  local stack=$7							;
-  local targets="$8"							;
-  local url=$9								;
+	local branch=$1							;
+	local export="$2"						;
+	local file=$3							;
+	local log=$4							;
+	local path=$5							;
+	local sleep=$6							;
+	local stack=$7							;
+	local targets="$8"						;
+	local url=$9							;
                                                                         #
-  local uuid=$( uuidgen )						;
+	local uuid=$( uuidgen )						;
                                                                         #
-  path=${uuid}/${path}							;
+	path=${uuid}/${path}						;
                                                                         #
-  local command="                                                       \
-    ${export}                                                           \
-    &&                                                                  \
-    path=${path}                                                        \
-    &&                                                                  \
-    git clone                                                           \
-      --single-branch --branch ${branch}                                \
-      https://${url}                                                    \
-      ${uuid}                                                           \
-    &&                                                                  \
-    chmod +x ${path}/${file}                                            \
-    &&                                                                  \
-    ${path}/${file}                                                     \
-      2>& 1                                                             \
-    |                                                                   \
-    tee --append ${log} 						\
-    &&                                                                  \
-    rm --force --recursive ${uuid}                                      \
-  "									;
+	local command=" 						\
+		${export}                                                           \
+		&&                                                                  \
+		path=${path}                                                        \
+		&&                                                                  \
+		git clone                                                           \
+			--single-branch --branch ${branch}                                \
+			https://${url}                                                    \
+			${uuid}                                                           \
+		&&                                                                  \
+		chmod +x ${path}/${file}                                            \
+		&&                                                                  \
+		${path}/${file}                                                     \
+			2>& 1                                                             \
+		|                                                                   \
+		tee --append ${log} 						\
+		&&                                                                  \
+		rm --force --recursive ${uuid}                                      \
+	"									;
                                                                         #
-  for target in ${targets}                                              ;
-  do 									\
-    _send_list_command "${command}" ${sleep} ${stack} ${target}		&
-  done                                                                  ;
+	for target in ${targets}                                              ;
+		do 									\
+			_send_list_command "${command}" ${sleep} ${stack} ${target}		&
+		done                                                                  ;
                                                                         #
 }									;
 #########################################################################
 function _send_list_command_targets { 					\
-  local command="$1"							;
-  local sleep=$2							;
-  local stack=$3							;
-  local targets="$4"							;
-  for target in ${targets}                                              ;
-  do 									\
-    _send_list_command "${command}" ${sleep} ${stack} ${target}		&
-  done                                                                  ;
+	local command="$1"							;
+	local sleep=$2							;
+	local stack=$3							;
+	local targets="$4"							;
+	for target in ${targets}                                              ;
+		do 									\
+			_send_list_command "${command}" ${sleep} ${stack} ${target}		&
+		done                                                                  ;
 }									;
 #########################################################################
 function _send_list_command_targets_wait { 				\
                                                                         #
-  local command="$1"							;
-  local sleep=$2							;
-  local stack=$3							;
-  local targets="$4"							;
+	local command="$1"							;
+	local sleep=$2							;
+	local stack=$3							;
+	local targets="$4"							;
                                                                         #
-  for target in ${targets}                                              ;
-  do 									\
-    while true								;
-    do 									\
-      output="$( _send_list_command "${command}" ${sleep} ${stack} ${target} )"	;
-      echo "${output}" | grep -q ERROR && continue			;
-      echo "${output}" | grep [a-zA-Z0-9] && break			;
-    done								;
-  done                                                                  ;
+	for target in ${targets}                                              ;
+		do 									\
+			while true								;
+				do 									\
+					output="$( _send_list_command "${command}" ${sleep} ${stack} ${target} )"	;
+					echo "${output}" | grep -q ERROR && continue			;
+					echo "${output}" | grep [a-zA-Z0-9] && break			;
+				done								;
+		done                                                                  ;
                                                                         #
 }									;
 #########################################################################
 function _wait_service_targets { 					\
                                                                         #
-  local service=$1							;
-  local sleep=$2							;
-  local stack=$3							;
-  local targets="$4"							;
+	local service=$1							;
+	local sleep=$2							;
+	local stack=$3							;
+	local targets="$4"							;
                                                                         #
-  local command=" 							\
-    service ${service} status 2> /dev/null | grep running 		\
-  "                                                                     ;
+	local command=" 							\
+		service ${service} status 2> /dev/null | grep running 		\
+	"                                                                     ;
                                                                         #
-  _send_list_command_targets_wait 					\
-  	"${command}" 							\
-	${sleep} 							\
-	${stack} 							\
-	"${targets}" 							\
+	_send_list_command_targets_wait 					\
+		"${command}" 							\
+		${sleep} 							\
+		${stack} 							\
+		"${targets}" 							\
 									;
 }									;
 #########################################################################
