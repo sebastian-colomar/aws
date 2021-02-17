aa1292a4f4fc396fa0de89be0fb36bf064720a6f
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-17 14:11:40 +0100

    Update init-orchestrator-workers.sh

--- bin/init-orchestrator-workers.sh
+++ bin/init-orchestrator-workers.sh
@@ -1,17 +1,11 @@
-#!/bin/bash -x
+#!/bin/sh
 #########################################################################
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
 set -x									;
 #########################################################################
-test -n "${engine}"		|| exit 101                             ;
-test -n "${HostedZoneName}"	|| exit 102                             ;
 test -n "${mode}"		|| exit 103                             ;
-test -n "${os}"			|| exit 104                             ;
-test -n "${stack}"		|| exit 105                             ;
-test -n "${version_major}"	|| exit 106                             ;
-test -n "${version_minor}"	|| exit 107                             ;
 #########################################################################
 file=common-functions.sh                                                ;
 path=lib                                                                ;
