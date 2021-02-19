15c058d2b16ff2af5c59ffd9e72a45b1fdc08cfa
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-19 02:37:42 +0100

    Update install-containerd-rhel8.sh

--- bin/install-containerd-rhel8.sh
+++ bin/install-containerd-rhel8.sh
@@ -41,7 +41,7 @@ do                                                                      \
                 &&                                                      \
                 break                                                   ;
                 sudo ${command} install -y                              \
-                        ${package                                       \
+                        ${package}                                      \
                                                                         ;
                 sleep ${sleep}                                          ;
         done                                                            ;
