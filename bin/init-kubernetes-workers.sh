321425200a5bf8f8678533a7dd1cf86a0b2b3c50
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-19 10:33:55 +0100

    Update init-kubernetes-workers.sh

--- bin/init-kubernetes-workers.sh
+++ bin/init-kubernetes-workers.sh
@@ -23,18 +23,18 @@ sleep=10								;
 url=${domain}/${username}/${repository}					;
 #########################################################################
 service=${engine}							;
-targets="								\
-	InstanceWorker1 						\
-	InstanceWorker2 						\
-	InstanceWorker3 						\
+targets="                                                               \
+        InstanceWorker1                                                 \
+        InstanceWorker2                                                 \
+        InstanceWorker3                                                 \
 "									;
 #########################################################################
 file=install-${service}-${os}.sh					;
 log=/tmp/${file}.log							;
 #########################################################################
-export=" 								\
-	export version_major=${version_major} 				\
-"									;
+export="                                                                \
+        export version_major=${version_major}                           \
+"                                                                       ;
 #########################################################################
 _send_list_command_remote 						\
 	${branch} 							\
@@ -49,21 +49,21 @@ _send_list_command_remote 						\
 									;
 #########################################################################
 service=kubelet								;
-targets="								\
-	InstanceWorker1 						\
-	InstanceWorker2 						\
-	InstanceWorker3 						\
+targets="                                                               \
+        InstanceWorker1                                                 \
+        InstanceWorker2                                                 \
+        InstanceWorker3                                                 \
 "									;
 #########################################################################
 file=install-${service}-${os}.sh					;
 log=/tmp/${file}.log							;
 #########################################################################
-export=" 								\
-	export engine=${engine} 					\
-	&& 								\
-	export version_major=${version_major} 				\
-	&& 								\
-	export version_minor=${version_minor} 				\
+export="                                                                \
+        export engine=${engine}                                         \
+        &&                                                              \
+        export version_major=${version_major}                           \
+        &&                                                              \
+        export version_minor=${version_minor}                           \
 "									;
 #########################################################################
 _send_list_command_remote 						\
@@ -84,21 +84,21 @@ for instance in 							\
 	InstanceMaster3 						\
 									;
 do 									\
-	eval ${instance}="$( 						\
-		aws ec2 describe-instances 				\
-			--filters 					\
-	Name=tag:"aws:cloudformation:stack-name",Values="${stack}" 	\
-	Name=tag:"aws:cloudformation:logical-id",Values="${instance}" 	\
-			--query 					\
-			Reservations[].Instances[].PrivateIpAddress 	\
-			--output 					\
-				text 					\
-	)"								;
+	eval ${instance}="$(                                            \
+                aws ec2 describe-instances                              \
+                        --filters                                       \
+        Name=tag:"aws:cloudformation:stack-name",Values="${stack}"      \
+        Name=tag:"aws:cloudformation:logical-id",Values="${instance}"   \
+                        --query                                         \
+                        Reservations[].Instances[].PrivateIpAddress     \
+                        --output                                        \
+                                text                                    \
+        )"								;
 done									;
 #########################################################################
 role=leader								;
-targets="								\
-	InstanceMaster1							\
+targets="                                                               \
+        InstanceMaster1                                                 \
 "									;
 #########################################################################
 file=init-${mode}-${role}.sh						;
@@ -106,131 +106,131 @@ log=/tmp/${file}.log							;
 #########################################################################
 token=discovery-token-ca-cert-hash					;
 #########################################################################
-command="								\
-	grep --max-count 1 						\
-		${token} 						\
-		${log}							\
+command="                                                               \
+        grep --max-count 1                                              \
+                ${token}                                                \
+                ${log}                                                  \
 "									;
-token_discovery=$( 							\
-	_encode_string "						\
-		$(							\
-			_send_list_command_targets_wait 		\
-				"${command}" 				\
-				${sleep} 				\
-				${stack} 				\
-				"${targets}" 				\
-		)							\
-	"								;
+token_discovery=$(                                                      \
+        _encode_string "                                                \
+                $(                                                      \
+                        _send_list_command_targets_wait                 \
+                                "${command}"                            \
+                                ${sleep}                                \
+                                ${stack}                                \
+                                "${targets}"                            \
+                )                                                       \
+       "                                                                ;
 )									;
 #########################################################################
 token=kubeadm.*join							;
 #########################################################################
-command="								\
-	grep --max-count 1 						\
-		${token} 						\
-		${log}							\
+command="                                                               \
+        grep --max-count 1                                              \
+                ${token}                                                \
+                ${log}                                                  \
 "									;
-token_token=$( 								\
-	_encode_string "						\
-		$(							\
-			_send_list_command_targets_wait 		\
-				"${command}" 				\
-				${sleep} 				\
-				${stack} 				\
-				"${targets}" 				\
-		)							\
-	"								;
+token_token=$(                                                          \
+        _encode_string "                                                \
+                $(                                                      \
+                        _send_list_command_targets_wait                 \
+                                "${command}"                            \
+                                ${sleep}                                \
+                                ${stack}                                \
+                                "${targets}"                            \
+                )                                                       \
+        "                                                               ;
 )									;
 #########################################################################
 role=worker								;
-targets="								\
-	InstanceWorker1 						\
-	InstanceWorker2 						\
-	InstanceWorker3 						\
+targets="                                                               \
+        InstanceWorker1                                                 \
+        InstanceWorker2                                                 \
+        InstanceWorker3                                                 \
 "									;
 #########################################################################
 file=init-${mode}-${role}.sh						;
 log=/tmp/${file}.log							;
 #########################################################################
-export=" 								\
-	export InstanceMaster1=${InstanceMaster1} 			\
-	&& 								\
-	export kube=${kube} 						\
-	&& 								\
-	export log=${log} 						\
-	&& 								\
-	export token_discovery=${token_discovery} 			\
-	&& 								\
-	export token_token=${token_token} 				\
+export="                                                                \
+        export InstanceMaster1=${InstanceMaster1}                       \
+        &&                                                              \
+        export kube=${kube}                                             \
+        &&                                                              \
+        export log=${log}                                               \
+        &&                                                              \
+        export token_discovery=${token_discovery}                       \
+        &&                                                              \
+        export token_token=${token_token}                               \
 "									;
 #########################################################################
-_send_list_command_remote 						\
-	${branch} 							\
-	"${export}" 							\
-	${file} 							\
-	${log} 								\
-	${path} 							\
-	${sleep} 							\
-	${stack} 							\
-	"${targets}" 							\
-	${url} 								\
-									;
+_send_list_command_remote                                               \
+        ${branch}                                                       \
+        "${export}"                                                     \
+        ${file}                                                         \
+        ${log}                                                          \
+        ${path}                                                         \
+        ${sleep}                                                        \
+        ${stack}                                                        \
+        "${targets}"                                                    \
+        ${url}                                                          \
+                                                                        ;
 #########################################################################
 service=docker								;
-targets="								\
-	InstanceWorker1 						\
-	InstanceWorker2 						\
-	InstanceWorker3 						\
+targets="                                                               \
+        InstanceWorker1                                                 \
+        InstanceWorker2                                                 \
+        InstanceWorker3                                                 \
 "									;
 #########################################################################
 file=install-${service}-${os}.sh					;
 log=/tmp/${file}.log							;
 #########################################################################
-export=" 								\
-  export engine=${engine}						\
+export="                                                                \
+        export engine=${engine}                                         \
 "									;
 #########################################################################
-_send_list_command_remote 						\
-	${branch} 							\
-	"${export}" 							\
-	${file} 							\
-	${log} 								\
-	${path} 							\
-	${sleep} 							\
-	${stack} 							\
-	"${targets}" 							\
-	${url} 								\
-									;
+_send_list_command_remote                                               \
+        ${branch}                                                       \
+        "${export}"                                                     \
+        ${file}                                                         \
+        ${log}                                                          \
+        ${path}                                                         \
+        ${sleep}                                                        \
+        ${stack}                                                        \
+        "${targets}"                                                    \
+        ${url}                                                          \
+                                                                        ;
 #########################################################################
 service=kube-lb								;
-targets="								\
-	InstanceWorker1 						\
-	InstanceWorker2 						\
-	InstanceWorker3 						\
+targets="                                                               \
+        InstanceWorker1                                                 \
+        InstanceWorker2                                                 \
+        InstanceWorker3                                                 \
 "									;
 #########################################################################
 file=install-${service}.sh						;
 log=/tmp/${file}.log							;
 #########################################################################
-export=" 								\
-	export InstanceMaster1=${InstanceMaster1} 			\
-	&& 								\
-	export InstanceMaster2=${InstanceMaster2} 			\
-	&& 								\
-	export InstanceMaster3=${InstanceMaster3} 			\
-	&& 								\
-	export kube=${kube} 						\
-"									;
+export="                                                                \
+        export InstanceMaster1=${InstanceMaster1}                       \
+        &&                                                              \
+        export InstanceMaster2=${InstanceMaster2}                       \
+        &&                                                              \
+        export InstanceMaster3=${InstanceMaster3}                       \
+        &&                                                              \
+        export kube=${kube}                                             \
+"                                                                       ;
 #########################################################################
-_send_list_command_remote 						\
-	${branch} 							\
-	"${export}" 							\
-	${file} 							\
-	${log} 								\
-	${path} 							\
-	${sleep} 							\
-	${stack} 							\
-	"${targets}" 							\
-	${url} 								\
-									;
+_send_list_command_remote                                               \
+        ${branch}                                                       \
+        "${export}"                                                     \
+        ${file}                                                         \
+        ${log}                                                          \
+        ${path}                                                         \
+        ${sleep}                                                        \
+        ${stack}                                                        \
+        "${targets}"                                                    \
+        ${url}                                                          \
+                                                                        ;
 #########################################################################
